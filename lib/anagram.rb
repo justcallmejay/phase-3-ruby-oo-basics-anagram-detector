270539508ab2a0a8b4d6e7e8d0cf386c5c1f84c9
jay <jaypifiverr@gmail.com>
2023-01-06 18:33:29 -0500

    commit tree 7033b8be942c5493f49396c8b3d47155bde24295

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :word
+
+    #thing is a local variable
+    #instance variable purpose is to access it outside initialize method
+    def initialize(thing)
+        @word = thing
+    end
+
+    def match(array)
+
+        input_word = @word.split('')
+
+        array.filter do |t| 
+            t.split('').sort == input_word.sort
+            # t.split('').sort == input_word.sort
+            # puts 'match'
+        end
+    end
+end
